2e355a580f8311e5b7191d597324403d7feb86cd
kaushikp092 <kaushikpatil092@gmail.com>
2025-12-09 16:28:29 +0530

    retrieving tasks form local storage if there is before

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-const todoInput = document.getElementById('todo-input');
-const addTaskBtn = document.getElementById('add-task-btn');
-const todoList = document.getElementById('todo-list');
+document.addEventListener("DOMContentLoaded", () => { // Ensures the DOM is fully loaded before running the script using DOMContentLoaded event
+    const todoInput = document.getElementById('todo-input');
+    const addTaskBtn = document.getElementById('add-task-btn');
+    const todoList = document.getElementById('todo-list');
 
-let tasks = [];
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || []; // Retrieving tasks from local storage or initializing an empty array
 
-addTaskBtn.addEventListener('click', () => {
-    const taskText = todoInput.value.trim(); // Get the input value and trim whitespace && Grabing input value using todoinput
-    if(taskText === "") return;// Prevent adding empty tasks && Nothing happens on empty input
+    tasks.forEach(task => renderTask(task));// Rendering each task on the UI
 
-    // if the task is there adding unqiue id 
+    addTaskBtn.addEventListener('click', () => {
+        const taskText = todoInput.value.trim(); // Get the input value and trim whitespace && Grabing input value using todoinput
+        if (taskText === "") return;// Prevent adding empty tasks && Nothing happens on empty input
 
-    const newTask = {
-        id: Date.now(), // Unique ID based on timestamp
-        text: taskText,
-        completed: false
+        // if the task is there adding unqiue id 
+
+        const newTask = {
+            id: Date.now(), // Unique ID based on timestamp
+            text: taskText,
+            completed: false
+        }
+
+        tasks.push(newTask); // Add the new task to the tasks array
+        saveTask();// Save tasks to local storage using localStorage api /* after adding new task to the local storage it's rewrite the whole array tasks*/
+        todoInput.value = ""; // Clear the input field after adding the task
+        console.log(tasks);
+
+    });
+
+    function renderTask(task) { //this function take local storage arr tasks and render on the ui
+        console.log(task);
     }
 
-    tasks.push(newTask); // Add the new task to the tasks array
-    saveTask();// Save tasks to local storage using localStorage api /* after adding new task to the local storage it's rewrite the whole array tasks*/
-    todoInput.value = ""; // Clear the input field after adding the task
-    console.log(tasks);
-    
+    function saveTask() {
+        localStorage.setItem('tasks', JSON.stringify(tasks));// Saving tasks to local storage 
+    }
 });
-
-function saveTask() {
-    localStorage.setItem('tasks', JSON.stringify(tasks));// Saving tasks to local storage 
-}
